28392d01162798882222ba2e0bb179c0ad4bc983
Benjamin DiGregorio <ben@digregor.io>
2021-11-01 22:05:56 -0700

    Add particle effects for rocket thrusters

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -4,10 +4,13 @@ public class Movement : MonoBehaviour {
     [SerializeField] float mainThrust = 0;
     [SerializeField] float rotationThrust = 0;
     [SerializeField] AudioClip rocketThrustSfx;
+    [SerializeField] ParticleSystem mainThrusterPFX;
+    [SerializeField] ParticleSystem leftThrusterPFX;
+    [SerializeField] ParticleSystem rightThrusterPFX;
 
     private Rigidbody _rigidbody;
-    public AudioSource _audioSource;
-    
+    private AudioSource _audioSource;
+
 
     void Start() {
         _rigidbody = GetComponent<Rigidbody>();
@@ -24,17 +27,30 @@ public class Movement : MonoBehaviour {
             _rigidbody.AddRelativeForce(mainThrust * Time.deltaTime * Vector3.up);
             if (!_audioSource.isPlaying) {
                 _audioSource.PlayOneShot(rocketThrustSfx);
+                if (!mainThrusterPFX.isPlaying) {
+                    mainThrusterPFX.Play();
+                }
             }
         } else {
             _audioSource.Stop();
+            mainThrusterPFX.Stop();
         }
     }
 
     private void ProcessRotation() {
         if (Input.GetKey(KeyCode.A)) {
             ApplyRotation(rotationThrust);
+            if (!rightThrusterPFX.isPlaying) {
+                rightThrusterPFX.Play();
+            }
         } else if (Input.GetKey(KeyCode.D)) {
             ApplyRotation(-rotationThrust);
+            if (!leftThrusterPFX.isPlaying) {
+                leftThrusterPFX.Play();
+            }
+        } else {
+            leftThrusterPFX.Stop();
+            rightThrusterPFX.Stop();
         }
     }
 
