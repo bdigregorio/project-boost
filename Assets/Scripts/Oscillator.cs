e9e27a1ba5c6491ea7d354a4d88c27c42850a436
Benjamin DiGregorio <ben@digregor.io>
2021-11-04 21:51:33 -0700

    Add oscillator script capability

---
+++ Assets/Scripts/Oscillator.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class Oscillator : MonoBehaviour {
+
+    Vector3 startingPosition;
+    private float tau;
+    [SerializeField] private float period;
+    [SerializeField] Vector3 movementVector;
+
+    void Start() {
+        startingPosition = transform.position;
+        tau = Mathf.PI * 2;
+    }
+
+    void Update() {
+        HandleOscillation();
+    }
+
+    void HandleOscillation() {
+        var cycles = Time.time  * tau / period; // input scaled properly for the period
+        var sineOutput = Mathf.Sin(cycles); // from -1 to 1
+        var movementFactor = (sineOutput + 1f) / 2f; // rescaled from 0 to 1 to calculate offset
+        var offset = movementFactor * movementVector; 
+        transform.position = startingPosition + offset;
+    }
+}
